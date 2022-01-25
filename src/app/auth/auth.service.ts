90530bb52ff06f1ff98dbbf8c7c1b507d4175018
Matt <matt@stallings.us>
2022-01-25 07:52:44 -0600

    Implemented auth guard

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -19,6 +19,7 @@ export interface AuthResponseData {
 })
 export class AuthService {
   user = new BehaviorSubject<User>(null);
+  private tokenExpirationTimer: any;
 
   constructor(private http: HttpClient) {}
 
@@ -47,6 +48,12 @@ export class AuthService {
 
   logout() {
     this.user.next(null);
+    localStorage.removeItem('userData');
+    
+    if(this.tokenExpirationTimer) {
+      clearTimeout(this.tokenExpirationTimer);
+      this.tokenExpirationTimer = null;
+    }
   }
 
   private handleError(errorRes: HttpErrorResponse) {
@@ -87,13 +94,20 @@ export class AuthService {
 
      if(loadedUser.token) {
        this.user.next(loadedUser);
+       const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+       this.autoLogout(expirationDuration);
      }
+  }
+
+  autoLogout(expirationDuration: number) {
+    this.tokenExpirationTimer = setTimeout(() => this.logout(), expirationDuration)
   }
 
   private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
     const expirationDate = new Date(new Date().getTime() + +expiresIn * 1000);
     const user = new User(email, userId, token, expirationDate);
     this.user.next(user);
+    this.autoLogout(expiresIn * 1000);
     localStorage.setItem('userData', JSON.stringify(user));
   }
 }
